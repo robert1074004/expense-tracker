e92e318740be3971ea1e735c98b4c755b6dd675d
robert1074004 <abc50113@yahoo.com.tw>
2022-06-02 13:29:36 +0800

    feat: add dotenv & mongoose

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 const express = require('express')
+const mongoose = require('mongoose') // 載入 mongoose
+mongoose.connect(process.env.MONGODB_URi, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
 const app = express()
+
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+  console.log(process.env.MONGODB_URi)
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 // 設定首頁路由
 app.get('/', (req, res) => {
