ecfc567479f955781de3fc7b9d4456dc4592d526
robert1074004 <abc50113@yahoo.com.tw>
2022-06-25 14:33:58 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,17 +1,10 @@
 
-const CATEGORY = {
-  家居物業: "https://fontawesome.com/icons/home?style=solid",
-  交通出行: "https://fontawesome.com/icons/shuttle-van?style=solid",
-  休閒娛樂: "https://fontawesome.com/icons/grin-beam?style=solid",
-  餐飲食品: "https://fontawesome.com/icons/utensils?style=solid",
-  其他: "https://fontawesome.com/icons/pen?style=solid"
-}
+
 const express = require('express')
-
 const exphbs = require('express-handlebars')
-
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const session = require('express-session')
 const routes = require('./routes')
 require('./config/mongoose')
 const app = express()
@@ -19,6 +12,12 @@ const app = express()
 
 app.engine('hbs',exphbs({defaultLayout:'main',extname:'.hbs'}))
 app.set('view engine','hbs')
+
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave:false,
+  saveUninitialized: true
+}))
 
 app.use(express.static('public'))
 
