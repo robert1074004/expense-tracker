b8866d358dd61f57d788e0afd16e7a34f8071913
robert1074004 <abc50113@yahoo.com.tw>
2022-06-28 14:44:06 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const methodOverride = require('method-override')
 const session = require('express-session')
 const routes = require('./routes')
 const usePassport = require('./config/passport')
+const flash = require('connect-flash')
 require('./config/mongoose')
 const app = express()
 
@@ -28,9 +29,13 @@ app.use(methodOverride('_method'))
 
 usePassport(app)
 
+app.use(flash())
+
 app.use((req,res,next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
   next()
 })
 
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -19,22 +19,33 @@ router.get('/register',(req,res) => {
 
 router.post('/register',(req,res) => {
     const {name,email,password,confirmPassword} = req.body
+    let errors = []
+    if (!name.trim() || !email.trim() || !password.trim() || !confirmPassword.trim()) {
+        
+        errors.push({message:'所有欄位都是必填。'})
+    }
+    if (password !== confirmPassword) {
+        errors.push({message: '密碼與確認密碼不相符!'})
+    }
+    if (errors.length) {
+        return res.render('register',{errors,name,email,password,confirmPassword})
+    }
     User.findOne({email}).then(user => {
-
         if (user) {
-            console.log('User already exists.')
-            res.render('register',{name,email,password,confirmPassword})
-        } else {
-            return User.create({name,email,password})
-                .then(() => res.redirect('/'))
-                .catch(err => console.log(err))
-        }
+            errors.push({message:'這個Email已經註冊過了。'})
+            return res.render('register',{name,email,password,confirmPassword,errors})
+        } 
+        return User.create({name,email,password})
+                    .then(() => res.redirect('/'))
+                    .catch(err => console.log(err))
+        
     })
     .catch(err => console.log(err))
 })
 
 router.get('/logout',(req,res) => {
     req.logout()
+    req.flash('success_msg','你已經成功登出。')
     res.redirect('/users/login')
 })
 
